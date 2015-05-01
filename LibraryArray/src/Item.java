24eea030add9a8dbff1ef786bda29ec6628e31f1
ccarmona9800 <ccarmona9800@001LAB102010551.neisd.ad>
2015-05-01 15:20:28 -0500

    Interface additions

--- LibraryArray/src/Item.java
+++ LibraryArray/src/Item.java
@@ -1,4 +1,15 @@
 
 public interface Item {
+	
+  public void title ();
+   
+  public void author ();
+  
+  public void genre ();
+  
+  public void checkout ();
+  
+  public void whoisit ();
+
 
 }
