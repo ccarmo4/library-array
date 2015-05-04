e857c69dd15c1d8d0477ad20a61b2788acc2f33b
ccarmona9800 <ccarmona9800@001LAB102010551.neisd.ad>
2015-05-04 15:29:02 -0500

    new class, methods

---
+++ LibraryArray/src/Books.java
@@ -0,0 +1,34 @@
+
+public class Books implements Item{
+
+	
+	public String getTitle() {
+		
+		return null;
+	}
+
+	
+	public String getAuthor() {
+		
+		return null;
+	}
+
+	
+	public String getGenre() {
+		
+		return null;
+	}
+
+	
+	public void checkout() {
+		
+		
+	}
+
+	
+	public String whoisit() {
+		
+		return null;
+	}
+
+}
