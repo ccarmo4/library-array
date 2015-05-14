36d8aff6504516adc140a39a8ded008132030686
kmarvellotr <kmnasa@gmail.com>
2015-05-14 15:34:53 -0500

    Update Library.java
    import java.util.Scanner;
    
    
    public class Library {
    	static int minCustomers = 100;
    	static Customer[] customers = new Customer[1];
        static Book[] books = new Book[3];
        
    	
    	//Create a scanner object each for text and numbers
        private static Scanner fInput = new Scanner(System.in);
    	private static Scanner lInput = new Scanner(System.in);
    	private Object customer;
    	private static Scanner tInput = new Scanner(System.in);
    	private static Scanner nInput = new Scanner(System.in);
    	private Object book;
    	
    	
    	
    	public static void main(String[] args) {
    		
    		
    		
    		addCustomers();
    		
    
    		for (int i = 0; i < customers.length; i++)
    		{
    			System.out.println(customers[i].getFname() + " " + customers[i].getLname());
    		}
    		
    		
    		//Add the books
    				addBooks();
    				
    				for (int i = 0; i < books.length; i++ )
    				{
    					System.out.println(books[i].getTitle() + ", by " + books[i].getAuthor());
    				}
    				
    				/*do 
    				{
    				System.out.println("What is the maximum amount of customers you expect?");
    				} while (!(Integer.parseInt(tInput.nextLine())>=minCustomers));
    				*/
    		
    
    		
    		
    	}
    	
    	private static void addCustomers()
    	{
    		for (int i = 0; i < customers.length; i++)
    		{
    			customers[i] = new Customer();
    			System.out.println("Enter your First Name");
    			customers[i].setFname(fInput.nextLine());
    			System.out.println("Enter your Last Name");
    			customers[i].setLName(lInput.nextLine());
    		}
    		
    		
    		
    	} 
    	
    	private static void addBooks()
    	{
    		for (int i = 0; i < books.length; i++ )
    		{
    			books[i] = new Book();
    			
    			System.out.println("Enter a Book Name");
    			
    			books[i].setTitle(tInput.nextLine());
    			
    			System.out.println("Enter the Author's Name");
    			
    			books[i].setAuthor(tInput.nextLine());
    			
    		}
    		
    
    	}
    
    }

--- LibraryArray/src/Library.java
+++ LibraryArray/src/Library.java
@@ -1,43 +1,69 @@
-package Library;
+
 
 import java.util.Scanner;
 
 
 public class Library {
 	static int minCustomers = 100;
-    static Book[] books = new Book[10];
+	static Customer[] customers = new Customer[1];
+    static Book[] books = new Book[3];
+    
 	
 	//Create a scanner object each for text and numbers
+    private static Scanner fInput = new Scanner(System.in);
+	private static Scanner lInput = new Scanner(System.in);
+	private Object customer;
 	private static Scanner tInput = new Scanner(System.in);
 	private static Scanner nInput = new Scanner(System.in);
+	private Object book;
+	
 	
 	
 	public static void main(String[] args) {
 		
-		//Add the books
-		addBooks();
 		
-		for (int i = 0; i < books.length; i++ )
+		
+		addCustomers();
+		
+
+		for (int i = 0; i < customers.length; i++)
 		{
-			System.out.println(books[i].getTitle() + ", by " + books[i].getAuthor());
+			System.out.println(customers[i].getFname() + " " + customers[i].getLname());
 		}
 		
-		/*do 
-		{
-		System.out.println("What is the maximum amount of customers you expect?");
-		} while (!(Integer.parseInt(tInput.nextLine())>=minCustomers));
-		*/
+		
+		//Add the books
+				addBooks();
+				
+				for (int i = 0; i < books.length; i++ )
+				{
+					System.out.println(books[i].getTitle() + ", by " + books[i].getAuthor());
+				}
+				
+				/*do 
+				{
+				System.out.println("What is the maximum amount of customers you expect?");
+				} while (!(Integer.parseInt(tInput.nextLine())>=minCustomers));
+				*/
+		
 
 		
 		
 	}
 	
-	private static int addCustomer()
+	private static void addCustomers()
 	{
+		for (int i = 0; i < customers.length; i++)
+		{
+			customers[i] = new Customer();
+			System.out.println("Enter your First Name");
+			customers[i].setFname(fInput.nextLine());
+			System.out.println("Enter your Last Name");
+			customers[i].setLName(lInput.nextLine());
+		}
 		
 		
 		
-		return 0;
 	} 
 	
 	private static void addBooks()
@@ -45,9 +71,13 @@ public class Library {
 		for (int i = 0; i < books.length; i++ )
 		{
 			books[i] = new Book();
-			System.out.println("Enter a book name");
+			
+			System.out.println("Enter a Book Name");
+			
 			books[i].setTitle(tInput.nextLine());
-			System.out.println("the Author?");
+			
+			System.out.println("Enter the Author's Name");
+			
 			books[i].setAuthor(tInput.nextLine());
 			
 		}
