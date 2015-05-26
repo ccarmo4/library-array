a035824223ce3f3c0e3588dc4a3e9b036cc8e1e9
ccarmona9800 <ccarmona9800@001LAB102010551.neisd.ad>
2015-05-26 15:17:19 -0500

    adding comments

--- LibraryArray/src/Books.java
+++ LibraryArray/src/Books.java
@@ -1,20 +1,25 @@
 
 public class Books implements Item{
-
+	//creates all the variables needed to check out a book
+	private String callNumber;
 	String title;
 	String author;
 	String genre; 
 	int price; 
+	private long custID;
+	private boolean checkedOut;
 	
+	//each method returns one of the variables above
 	public String getTitle() {
 		
-		return null;
+		
+		return title;
 	}
 
 	
 	public String getAuthor() {
 		
-		return null;
+		return author;
 	}
 
 	
@@ -33,6 +38,81 @@ public class Books implements Item{
 	public String whoisit() {
 		
 		return null;
+	}
+
+
+	@Override
+	public String Library() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public String Customer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	
+	public String Book() {
+		
+		return null;
+	}
+
+
+	
+	public String getCallNumber() {
+		
+		return callNumber;
+	}
+
+
+	
+	public void setCallNumber(String callNum) {
+		callNumber = callNum;
+		
+	}
+
+
+	
+	public boolean checkOut(long customerID) {
+		if(checkedOut)
+		return false;
+		
+		setCheckedOut (true);
+		setCustomerID (customerID);
+		return true;
+	}
+
+
+	
+	public void setCheckedOut(boolean checked) {
+		checkedOut = checked;
+		
+		
+	}
+
+
+	
+	public void setCustomerID(long customerID) {
+		
+		
+	}
+
+
+	
+	public boolean getCheckedOut() {
+	
+		return checkedOut;
+	}
+
+
+	
+	public long getCustomerID() {
+		
+		return custID;
 	}
 
 }
